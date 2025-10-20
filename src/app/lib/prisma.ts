00691934b67e00829ec4d62a34326ed97edd9503
Samad10jan <abdulsamad10jan@gmail.com>
2025-10-20 12:13:33 +0530

    First commit

---
+++ src/app/lib/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "../../../generated/prisma";
+
+
+const prisma = new PrismaClient()
+
+export default prisma;
+// use `prisma` in your application to read and write data in your DB
