33fec9f63f2d1228634ac47f4537918ba5d0b4fd
Samad10jan <abdulsamad10jan@gmail.com>
2026-03-03 12:45:00 +0530

    -

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*", // replace with your frontend domain in production
+  "Access-Control-Allow-Origin": "https://rakshak-gamma.vercel.app/", // replace with your frontend domain in production
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
